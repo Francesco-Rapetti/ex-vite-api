3fd6d61ad5b89d18e74941fa1fa83357b33427a2
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-16 13:30:07 +0100

    Remove api query

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-    apiUrl: "https://api.openbrewerydb.org/v1/breweries?by_country=ireland&per_page=10",
+    apiUrl: "https://api.openbrewerydb.org/v1/breweries",
     selectedType: null,
     breweryTypes: [],
     brewery: [],
